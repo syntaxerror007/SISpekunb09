510df3a439f016af0d522b092585fcbfe0b15e40
yosualb <yosualb@live.com>
2015-04-14 07:33:12 +0700

    dari Yosua

--- SISpekunWebService.php
+++ SISpekunWebService.php
@@ -138,11 +138,12 @@
 		//die("haha ".$idPeminjam);
 		if ($tipePeminjam == "Mahasiswa")
 		{
-			$query = mysqli_query($con, "SELECT MAHASISWA.Nama as NamaPeminjam, MAHASISWA.NPM as Identitas, PEMINJAMAN.No_Spekun FROM MAHASISWA,PEMINJAMAN WHERE MAHASISWA.NPM = PEMINJAMAN.NPM_Mahasiswa AND Tanggal = $tanggal AND Bulan = $bulan AND Tahun = $tahun AND (Status = NULL OR Status = 0)  AND MAHASISWA.NPM=$idPeminjam");
+			$query = mysqli_query($con, "SELECT MAHASISWA.Nama as NamaPeminjam, MAHASISWA.NPM as Identitas, PEMINJAMAN.No_Spekun FROM MAHASISWA,PEMINJAMAN WHERE MAHASISWA.NPM = PEMINJAMAN.NPM_Mahasiswa AND (Status = NULL OR Status = 0) AND MAHASISWA.NPM=$idPeminjam");
+			//AND PEMINJAMAN.Tanggal = $tanggal AND PEMINJAMAN.Bulan = $bulan AND PEMINJAMAN.Tahun = $tahun
 		}
 		else
 		{
-			$query = mysqli_query($con, "SELECT NON_MAHASISWA.Nama as NamaPeminjam, NON_MAHASISWA.No_KTP as Identitas, PEMINJAMAN.No_Spekun, NON_MAHASISWA.PEKERJAAN as Pekerjaan FROM NON_MAHASISWA,PEMINJAMAN WHERE NON_MAHASISWA.NPM = PEMINJAMAN.NPM_Mahasiswa AND Tanggal = $tanggal AND Bulan = $bulan AND Tahun = $tahun AND (Status = NULL or Status = 0) AND NON_MAHASISWA.No_KTP=$idPeminjam");
+			$query = mysqli_query($con, "SELECT NON_MAHASISWA.Nama as NamaPeminjam, NON_MAHASISWA.No_KTP as Identitas, PEMINJAMAN.No_Spekun, NON_MAHASISWA.PEKERJAAN as Pekerjaan FROM NON_MAHASISWA,PEMINJAMAN WHERE NON_MAHASISWA.NPM = PEMINJAMAN.NPM_Mahasiswa AND (Status = NULL or Status = 0) AND NON_MAHASISWA.No_KTP=$idPeminjam");
 		}
 		
 		return $query;
