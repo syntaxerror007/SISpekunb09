3a0eda9b8855dbc1ebb02f51b338954634e7a085
yosualb <yosualb@live.com>
2015-04-14 14:58:07 +0700

    dari Yosua

--- SISpekunWebService.php
+++ SISpekunWebService.php
@@ -138,6 +138,7 @@
 		$bulan = date("m");
 		$tahun = date("Y");
 		$jam_pengembalian = date("h:i:s");
+		return "INSERT INTO KERUSAKAN_SPEKUN (Detail, Hari, Tanggal, Bulan, Tahun, No_Spekun) VALUES ('$informasi', '$hari', '$tanggal', '$bulan', '$tahun', '$noSpekun')";
 		return array('status' => mysqli_query($con, "INSERT INTO KERUSAKAN_SPEKUN (Detail, Hari, Tanggal, Bulan, Tahun, No_Spekun) VALUES ('$informasi', '$hari', '$tanggal', '$bulan', '$tahun', '$noSpekun')"));
 	}
 	
