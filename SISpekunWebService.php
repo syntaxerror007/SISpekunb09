cc108fa01ce3cf36542ace61667ce0461cacc148
yosualb <yosualb@live.com>
2015-04-14 14:28:04 +0700

    dari Yosua

--- SISpekunWebService.php
+++ SISpekunWebService.php
@@ -85,7 +85,6 @@
 		$bulan = date("m");
 		$tahun = date("Y");
 		$jam_pengembalian = date("h:i:s");
-		//return $namaShelterKembali.$idPeminjam.$tipePeminjam;
 		if ($tipePeminjam == "Mahasiswa") {
 			$query = "UPDATE PEMINJAMAN SET Status = 1, Jam_Kembali = '$jam_pengembalian', Lokasi_Kembali = '$namaShelterKembali' WHERE Tanggal = '$tanggal' AND Bulan = '$bulan' AND Tahun = '$tahun' AND NPM_Mahasiswa = '$idPeminjam' AND Status = 0";
 			if(mysqli_query($con, $query)) {
@@ -96,8 +95,7 @@
 			}
 		}
 		else {
-			$query = "SELECT Nama FROM MAHASISWA";
-			//$query = "UPDATE PEMINJAMAN SET Status = 1, Jam_Kembali = '$jam_pengembalian', Lokasi_Kembali = '$namaShelterKembali' WHERE Tanggal = '$tanggal' AND Bulan = '$bulan' AND Tahun = '$tahun' AND ID_Non_Mahasiswa = '$idPeminjam' AND Status = 0";
+			$query = "UPDATE PEMINJAMAN SET Status = 1, Jam_Kembali = '$jam_pengembalian', Lokasi_Kembali = '$namaShelterKembali' WHERE Tanggal = '$tanggal' AND Bulan = '$bulan' AND Tahun = '$tahun' AND ID_Non_Mahasiswa = '$idPeminjam' AND Status = 0";
 			if(mysqli_query($con, $query)) {
 				return array('status'=>'1');
 			}
