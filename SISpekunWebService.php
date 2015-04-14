953a9084c719136b4c180b174154190b48051f6d
yosualb <yosualb@live.com>
2015-04-14 08:26:18 +0700

    dari Yosua

--- SISpekunWebService.php
+++ SISpekunWebService.php
@@ -140,7 +140,7 @@
 		$res;
 		if ($tipePeminjam == "Mahasiswa")
 		{
-			$res = mysqli_query($con, "SELECT * FROM PENJAGA_SHELTER WHERE Username = '$idPeminjam'");
+			$res = mysqli_query($con, "SELECT * FROM PENJAGA_SHELTER");
 			//$query = mysqli_query($con, "SELECT MAHASISWA.Nama as NamaPeminjam, MAHASISWA.NPM as Identitas, PEMINJAMAN.No_Spekun FROM MAHASISWA,PEMINJAMAN WHERE MAHASISWA.NPM = PEMINJAMAN.NPM_Mahasiswa AND (Status = NULL OR Status = 0) AND MAHASISWA.NPM=$idPeminjam");
 			//AND PEMINJAMAN.Tanggal = $tanggal AND PEMINJAMAN.Bulan = $bulan AND PEMINJAMAN.Tahun = $tahun
 			// while($row = mysqli_fetch_assoc($result))
@@ -151,7 +151,7 @@
 		}
 		else
 		{
-			$res = mysqli_query($con, "SELECT * FROM PENJAGA_SHELTER WHERE Username = '$idPeminjam'");
+			$res = mysqli_query($con, "SELECT * FROM PENJAGA_SHELTER");
 			//$query = mysqli_query($con, "SELECT NON_MAHASISWA.Nama as NamaPeminjam, NON_MAHASISWA.No_KTP as Identitas, PEMINJAMAN.No_Spekun, NON_MAHASISWA.PEKERJAAN as Pekerjaan FROM NON_MAHASISWA,PEMINJAMAN WHERE NON_MAHASISWA.NPM = PEMINJAMAN.NPM_Mahasiswa AND (Status = NULL or Status = 0) AND NON_MAHASISWA.No_KTP=$idPeminjam");
 			// while($row = mysqli_fetch_assoc($result))
 				// $test[] = $row;
