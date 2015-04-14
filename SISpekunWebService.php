f2ad48717782746fa36c3e4e8b243acd38dcf7f5
yosualb <yosualb@live.com>
2015-04-14 14:02:09 +0700

    dari Yosua

--- SISpekunWebService.php
+++ SISpekunWebService.php
@@ -79,10 +79,6 @@
 	
 	function DoPengembalian($namaShelterKembali, $idPeminjam, $tipePeminjam)
 	{
-		$namaShelterKembali = mysqli_real_escape_string($con, stripslashes($namaShelterKembali));
-		$idPeminjam = mysqli_real_escape_string($con, stripslashes($idPeminjam));
-		$tipePeminjam = mysqli_real_escape_string($con, stripslashes($tipePeminjam));
-		
 		$tanggal = date("d");
 		$bulan = date("m");
 		$tahun = date("Y");
