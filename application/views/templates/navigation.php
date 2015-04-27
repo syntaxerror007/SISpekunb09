cf1d4e9b22360b0616d05276d8c78978e57a5d8d
arvinsamuar <samuar.arvin@gmail.com>
2015-04-27 11:42:36 +0700

    Update navigation.php

--- application/views/templates/navigation.php
+++ application/views/templates/navigation.php
@@ -79,6 +79,18 @@
                 <!-- /.nav-second-level -->
             </li>
             <li>
+                <a href="#"><i class="fa fa-files-o fa-fw"></i> Peminjaman Khusus<span class="fa arrow"></span></a>
+                <ul class="nav nav-second-level">
+                    <li>
+        		 <a href="<?php echo site_url('formulir'); ?>"><i class="fa fa-table fa-fw"></i> Formulir Peminjaman</a>
+            	    </li>
+                    <li>
+        		 <a href="<?php echo site_url('listPeminjaman'); ?>"><i class="fa fa-table fa-fw"></i> List Peminjaman Khusus</a>
+            	    </li>
+                </ul>
+                <!-- /.nav-second-level -->
+            </li>
+            <li>
                 <a href="<?php echo site_url('formulir'); ?>"><i class="fa fa-table fa-fw"></i> Formulir Peminjaman</a>
             </li>
 
